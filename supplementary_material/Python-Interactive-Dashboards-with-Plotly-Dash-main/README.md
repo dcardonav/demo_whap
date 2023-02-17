bb46565c051fd3da898d57d7e42fe110215f35e8
David Cardona-Vasquez <dcardon@gmail.com>
2023-02-17 22:52:15 +0100

    with requirements.txt... again

--- supplementary_material/Python-Interactive-Dashboards-with-Plotly-Dash-main/README.md
+++
@@ -1,5 +0,0 @@
-
-
-
-# Python-Interactive-Dashboards-with-Plotly-Dash
-Python Interactive Dashboards with Plotly Dash, by Packt Publishing
--- supplementary_material/Python-Interactive-Dashboards-with-Plotly-Dash-main/creating_your_first_dashboard.py
+++
@@ -1,8 +0,0 @@
-from dash import Dash, html
-
-app = Dash()
-
-app.layout = html.Div('My Dashboard')
-
-if __name__ == '__main__':
-    app.run_server(debug=True)
